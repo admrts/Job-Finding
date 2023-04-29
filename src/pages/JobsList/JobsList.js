b7e1b38843db9db102d8928e25eaf8b1de2bb5bc
admrts <alidemirtas.7@icloud.com>
2023-04-29 18:58:24 +0300

    createPages

---
+++ src/pages/JobsList/JobsList.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const JobsList = () => {
+  return (
+    <View>
+      <Text>JobsList</Text>
+    </View>
+  );
+};
+
+export default JobsList;
