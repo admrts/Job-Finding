f9e6c6825c6a7f5029ebc4080c9458154cb54f1c
admrts <alidemirtas.7@icloud.com>
2023-04-29 19:10:36 +0300

    create custom fetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const { data } = await axios(url);
+
+      setData(data.results);
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return { data, error, loading };
+};
+export default useFetch;
