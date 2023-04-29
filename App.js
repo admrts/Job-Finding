86f8dfedc30969ef97d24baef65518d3399e4a3e
admrts <alidemirtas.7@icloud.com>
2023-04-29 19:05:43 +0300

    create drawer

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Favorites from "./src/pages/Favorites/Favorites";
+import JobsList from "./src/pages/JobsList/JobsList";
+import { NavigationContainer } from "@react-navigation/native";
+
+import "react-native-gesture-handler";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="JobsList" component={JobsList} />
+        <Drawer.Screen name="Favorites" component={Favorites} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f3f3",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
