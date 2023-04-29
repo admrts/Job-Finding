bd2b68a68b04d1d299da3368afc268b9ad94412d
admrts <alidemirtas.7@icloud.com>
2023-04-29 19:07:50 +0300

    create jobcard

---
+++ src/components/JobCard/JobCard.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const JobCard = () => {
+  return (
+    <View>
+      <Text>JobCard</Text>
+    </View>
+  );
+};
+
+export default JobCard;
